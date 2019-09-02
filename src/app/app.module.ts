470071cdf697c0c0d75dacb69f125f24dde31f0b
oplengs001 <oplengs@gmail.com>
2019-09-02 15:34:19 +0800

    push notif and icon change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {FCM} from '@ionic-native/fcm/ngx';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -35,6 +37,7 @@ import 'gl-ionic-background-video';
   providers: [
     StatusBar,
     SplashScreen,
+    FCM,
     AuthService,
     Geolocation,
     NativeGeocoder,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FCM } from '@ionic-native/fcm/ngx';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(){}    
-}
+  pushes: any = [];
+  constructor(private fcm: FCM, public plt: Platform) {
+    this.plt.ready()
+      .then(() => {
+        this.fcm.onNotification().subscribe(data => {
+          if (data.wasTapped) {
+            console.log("Received in background");
+          } else {
+            console.log("Received in foreground");
+          };
+        });
 
+        this.fcm.onTokenRefresh().subscribe(token => {
+          // Register your new token in your back-end if you want
+          // backend.registerToken(token);
+          console.log(token) 
+          console.log("fcm token for testing 1")
+        });
+      })
+  }
+  subscribeToTopic() {
+    this.fcm.subscribeToTopic('enappd');
+  }
+  getToken() {
+    this.fcm.getToken().then(token => {
+      console.log(token) 
+      console.log("fcm token for testing 2")
+      // Register your new token in your back-end if you want
+      // backend.registerToken(token);
+    });
+  }
+  unsubscribeFromTopic() {
+    this.fcm.unsubscribeFromTopic('enappd');
+  }
+}
