ae9a834989151687de1734e55b1b234e44b9f712
oplengs001 <oplengs@gmail.com>
2019-09-18 13:41:35 +0800

    chat service partials

--- src/app/messages/messages.page.ts
+++ src/app/messages/messages.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../services/chat/chat.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit {
+  chat$: Observable<any>;
+  newMsg: string;
 
-  constructor() { }
+  constructor(
+    public cs: ChatService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
+    const chatId = "hohoohohoho"
+    
+    const source = this.cs.get(chatId);
+    this.chat$ = this.cs.joinUsers(source);
+
+  }
+
+  submit(chatId) {
+    this.cs.sendMessage(chatId, this.newMsg);
+    this.newMsg = '';
+  }
+
+  trackByCreated(i, msg) {
+    return msg.createdAt;
   }
 
 }
